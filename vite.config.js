b1898b81953d86ccebe95b33476773626e1fc0dc
Z <zeta.hoffman@protonmail.com>
2023-09-13 19:55:51 -0600

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Proyecto-SecurityTrainingPlathform.github.io/',
+  plugins: [react()]
 })
